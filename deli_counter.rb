3b5123002e54704807a2911e2b98d6ed141c7600
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-18 20:36:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+require 'pry'
+
+def line(names)
+  if names.empty?
+    puts "The line is currently empty."
+  else 
+   positions = names.map.with_index(1) {|name, index| "#{index}. #{name}"}
+   stringified_positions = positions.join(" ")
+    puts "The line is currently: " + stringified_positions
+  end
+end
